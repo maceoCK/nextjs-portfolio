6b6732886021837f61d39320f69607dfb7181c08
maceoCK <maceo@vt.edu>
2024-08-13 16:52:02 -0400

    personal

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "maceock.me",
+    template: "%s | maceock.me",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "maceock.me",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://maceock.me",
+    siteName: "maceock.me",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://maceock.me/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Maceock",
     card: "summary_large_image",
   },
   icons: {
